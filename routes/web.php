14d01dd471c43db3e6d72c2bb262e9ee0f7732b9
yLukas077 <lukasmbs1234@gmail.com>
2023-12-16 22:51:37 -0300

    test param routes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/','MainController@Main' );
 
 Route::get('/about', 'AboutController@About');
 
 Route::get('/contact','ContactController@Contact');
+
+Route::get(
+    '/contact/{name}/{category_id}',
+    function(string $name = "Desconhecido", int $category_id = 1) {
+        echo "Listando: $name, $category_id";
+    }
+)->where('category_id', '[0-9]+')->where('name', '[A-Za-z]+');  
